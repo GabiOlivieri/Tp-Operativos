7262caf5e244758604f64bd0dbed66672a733768
Gabriel Olivieri <golivieri@frba.utn.edu.ar>
2022-04-21 23:02:42 -0300

    Agrego archivos de commit

--- kernel/include/kernel.h
+++ kernel/include/kernel.h
@@ -7,7 +7,38 @@
 #include <shared/socket.h>
 #include <shared/server.h>
 #include <commons/collections/list.h>
+#include <commons/config.h>
 
+typedef struct configuraciones {
+    char *ip_memoria;
+    u_int16_t puerto_memoria;
+    char *ip_cpu;
+    u_int16_t puerto_cpu_dispatch;
+    u_int16_t puerto_cpu_interrupt;
+    u_int16_t puerto_escucha;
+    char *algoritmo_planificacion;
+    u_int16_t estimacion_inicial;
+    double alfa;
+    u_int16_t grado_multiprogramacion;
+    u_int16_t tiempo_max_bloqueado;
+} t_configuraciones;
 
+/**
+* @NAME: leer_config
+* @DESC: lee los datos del config.
+*/
+void leer_config(t_config* config, t_configuraciones* configuraciones);
+
+/**
+* @NAME: liberar_memoria
+* @DESC: libera la memoria usada en este modulo.
+*/
+void liberar_memoria(t_log* logger, t_config* config , t_configuraciones* configuraciones , int servidor);
+
+/**
+* @NAME: nombre_free
+* @DESC: libera la memoria usada de nombre structura para almacenar los valores obtenidos del config.
+*/
+void configuraciones_free(t_configuraciones* configuraciones);
 
 #endif
--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -3,6 +3,27 @@
 int main(int argc, char* argv[]) {
 	///home/utnso/tp-2022-1c-Champagne-SO/kernel/kernel.log Harcodeada la ruta
     t_log* logger = log_create("./kernel.log","KERNEL", false , LOG_LEVEL_TRACE);
+    t_config* config = config_create("./kernel.conf");
+    t_configuraciones* configuraciones = malloc(sizeof(t_configuraciones));
+
+
+    char* config_properties[] = {
+            "IP_MEMORIA",
+            "PUERTO_MEMORIA",
+			"IP_CPU",
+			"PUERTO_CPU_DISPATCH",
+			"PUERTO_CPU_INTERRUPT",
+			"PUERTO_ESCUCHA",
+			"ALGORITMO_PLANIFICACION",
+			"ESTIMACION_INICIAL",
+			"ALFA",
+			"GRADO_MULTIPROGRAMACION",
+			"TIEMPO_MAXIMO_BLOQUEADO",
+            NULL
+        };
+
+    leer_config(config,configuraciones);
+
     int servidor = iniciar_servidor(logger , "un nombre" , "127.0.0.1" , "8000");
     int client_socket = esperar_cliente(logger , "un nombre" , servidor);
 
@@ -32,5 +53,32 @@ int main(int argc, char* argv[]) {
 			break;
 		}
 	}
+    liberar_memoria(logger,config,configuraciones,servidor);
+
     return 0;
+}
+
+void leer_config(t_config* config, t_configuraciones* configuraciones){
+	configuraciones->ip_memoria = config_get_string_value(config , "IP_MEMORIA");
+	configuraciones->puerto_memoria = config_get_int_value(config , "PUERTO_MEMORIA");
+	configuraciones->ip_cpu = config_get_string_value(config , "IP_CPU");
+	configuraciones->puerto_cpu_dispatch = config_get_int_value(config , "PUERTO_CPU_DISPATCH");
+	configuraciones->puerto_cpu_interrupt = config_get_int_value(config , "PUERTO_CPU_INTERRUPT");
+	configuraciones->puerto_escucha = config_get_int_value(config , "PUERTO_ESCUCHA");
+	configuraciones->algoritmo_planificacion = config_get_string_value(config , "ALGORITMO_PLANIFICACION");
+	configuraciones->estimacion_inicial = config_get_int_value(config , "ESTIMACION_INICIAL");
+	configuraciones->alfa = config_get_double_value(config , "ALFA");
+	configuraciones->grado_multiprogramacion = config_get_int_value(config , "GRADO_MULTIPROGRAMACION");
+	configuraciones->tiempo_max_bloqueado = config_get_int_value(config , "TIEMPO_MAXIMO_BLOQUEADO");
+}
+
+void liberar_memoria(t_log* logger, t_config* config , t_configuraciones* configuraciones , int servidor){
+    log_destroy(logger);
+    config_destroy(config);
+    configuraciones_free(configuraciones);
+    close(servidor);
+}
+
+void configuraciones_free(t_configuraciones* configuraciones){
+    free(configuraciones);
 }
